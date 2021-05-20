552568348aff8523dd0c0511010b14837587f8c0
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-05-20 14:35:41 -0400

    Delete archivo.js

--- archivo.js
+++
@@ -1,6 +0,0 @@
-{
-  "texto": [
-    "¿Ha padecido alguna de estas enfermedades o tiene conocimiento de sufir o tener algun hallazgo medico-clinico asociado a alguna de las siguientes enfermedades mentales o psiuiatricas o del comportamiento?",
-    "¿Ha padecido se le ha diagnosticado o tiene conocimiento de sufir o tener algun hallazgo medico-clinico asociado a alguna de las siguientes enfermedades mentales o psiuiatricas o del comportamiento?"
-  ]
-}
