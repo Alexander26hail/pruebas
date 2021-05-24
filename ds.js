cb493b87bd2af230e1c726508fa2acc8ee88b04c
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-05-24 12:52:16 -0400

    Update ds.js

--- ds.js
+++ ds.js
@@ -9,8 +9,20 @@
          "descripcion":"Enfermedades del sistema nervioso o que le afecten:"
       },
       {
-         "numero":2,
-         "descripcion":"¿Tiene fiebre?"
+         "numero":3,
+         "descripcion":"Enfermedades del sistema respiratorio:"
+      },
+      {
+         "numero":4,
+         "descripcion":"Enfermedades del corazón y sistema circulatorio:"
+      },
+      {
+         "numero":5,
+         "descripcion":"Enfermedades del sistema digestivo:"
+      },
+      {
+         "numero":6,
+         "Enfermedades ginecológicas y de las mamas:"
       }
    ]
 }
