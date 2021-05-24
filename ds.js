1204279a342be4b597dddcc2dcf071ea82f50e2e
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-05-24 12:59:47 -0400

    Add files via upload

---
+++ ds.js
@@ -0,0 +1,25 @@
+{
+   "result":[
+      {
+         "numero":1,
+         "descripcion":"Enfermedades mentales o psiquiátricas o del comportamiento:"
+      },
+      {
+         "numero":2,
+         "descripcion":"Enfermedades del sistema nervioso o que le afecten:"
+      },
+      {
+         "numero":3,
+         "descripcion":"Enfermedades del sistema respiratorio:"
+      },
+      {
+         "numero":4,
+         "descripcion":"Enfermedades del corazón y sistema circulatorio:"
+      },
+      {
+         "numero":5,
+         "descripcion":"Enfermedades del sistema digestivo:"
+      }
+   ]
+}
+
