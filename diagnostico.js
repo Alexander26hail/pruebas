5499904dbe2548277eb5a588dadc551674273bf2
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-07-08 14:44:36 -0400

    Add files via upload

---
+++ diagnostico.js
@@ -0,0 +1,16 @@
+{
+   "result":[
+      {
+         "BeneficiarioId":1,
+         "DetalleDeclaracionId":"1121141",
+		 "TipoBeneficiario":"Cotizante",
+		 "Cie10":"e10",
+		 "Intervencion":"si",
+		 "MesDiagnostico":"08-07-2021",
+		 "AnoDiagnostico":"2021",
+		 "ExclusionFcs":"no"
+		 "FechaExclusionFcs":"08-07-2021"
+      },
+      
+   ]
+}
