9a43036336306f7f6b001b3775a4d007f4a74a93
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-07-09 13:15:58 -0400

    Add files via upload

---
+++ bandeja.js
@@ -0,0 +1,33 @@
+
+  {
+    "result": [
+      {
+        "Folio": "1000101",
+        "NombreCotizante": "Agusto Fuentes",
+        "FechaCreacion": "01/07/2021",
+        "FechaVencimiento": "06/07/2021",
+        "FechaResoluccion": "06/07/2021"
+      },
+      {
+        "Folio": "1000102",
+        "NombreCotizante": "Felipe Fuentes",
+        "FechaCreacion": "02/07/2021",
+        "FechaVencimiento": "07/07/2021",
+        "FechaResoluccion": "07/07/2021"
+      },
+      {
+        "Folio": "1000103",
+        "NombreCotizante": "Oscar Fuentes",
+        "FechaCreacion": "03/07/2021",
+        "FechaVencimiento": "08/07/2021",
+        "FechaResoluccion": "08/07/2021"
+      },
+      {
+        "Folio": "1000104",
+        "NombreCotizante": "Pedro Fuentes",
+        "FechaCreacion": "04/07/2021",
+        "FechaVencimiento": "09/07/2021",
+        "FechaResoluccion": "09/07/2021"
+      }
+    ]
+  }
