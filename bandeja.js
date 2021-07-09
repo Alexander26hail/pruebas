e121dcc8da71d032f464373f386f908836494af8
Alexander26hail <44115219+Alexander26hail@users.noreply.github.com>
2021-07-09 11:16:14 -0400

    Add files via upload

---
+++ bandeja.js
@@ -0,0 +1,13 @@
+
+  {
+    "result": [
+      {
+        "Folio":"1000101",
+        "NombreCotizante": "Agusto Fuentes",
+        "FechaCreacion": "01/07/2021",
+        "FechaVencimiento": "06/07/2021",
+        "FechaResoluccion": "06/07/2021"
+      }
+    ]
+  }
+
